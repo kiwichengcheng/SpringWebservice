8eeddb9735fcbcdf37d09ae99ca8495b2c8b1cea
U6013046 <U6013046@U6013046-TPL-A.ten.thomsonreuters.com>
2015-02-09 17:20:07 +0800

    checkpoint

---
+++ src/main/java/com/reuters/cc/controller/WebServiceController.java
@@ -0,0 +1,15 @@
+package com.reuters.cc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class WebServiceController {
+
+	@RequestMapping(value="/Test", method=RequestMethod.PUT)
+	public String TestGet(){
+		return "{}";
+	}
+}
